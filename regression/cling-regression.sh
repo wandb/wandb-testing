9f2b84e699b274d9fdea6bf15c05052d19c40d71
Jeff Raubitschek <jeff@wandb.com>
2020-07-27 14:45:23 -0700

    add fd increase for dataframe tests

--- regression/cling-regression.sh
+++ regression/cling-regression.sh
@@ -1,1 +1,2 @@
+ulimit -n 4096
 ./regression.py --spec ::~broken --cli_base wandb-ng --cli_repo wandb/client-ng.git $* main/
--- regression/do-main-regression.sh
+++ regression/do-main-regression.sh
@@ -1,1 +1,2 @@
+ulimit -n 4096
 ./regression.py --spec ::~broken $* main/
