0fa01b7a9cca3ca04d71cee9195a48130123c5bc
Jeff Raubitschek <jeff@wandb.com>
2020-08-02 15:36:55 -0700

    add check to kerasfit

---
+++ regression/main/wandb-git/examples/tensorboard-tf2-kerasfit/check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Common code (refactor into regression framework at some point)
+import os
+import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
+project = os.environ.get("WANDB_PROJECT") or "regression"
+api = wandb.Api()
+last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
+assert last_run.summary_metrics["global_step"] == 4
+assert last_run.summary_metrics["train/global_step"] == 2
+assert last_run.summary_metrics["validation/global_step"] == 4
+assert last_run.summary_metrics["validation/epoch_loss"] > 0
+assert last_run.summary_metrics["validation/epoch_accuracy"] > 0
