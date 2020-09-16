ab145d73ca90d13897155b3fd8b4a55165a900e2
Jeff Raubitschek <jeff@wandb.com>
2020-09-16 16:05:26 -0700

    update cling

--- regression/cling-regression.sh
+++ regression/cling-regression.sh
@@ -1,2 +1,2 @@
 ulimit -n 4096
-./regression.py --spec ::~broken --cli_base wandb-ng --cli_repo wandb/client-ng.git $* main/
+./regression.py --spec ::~broken --cli_base wandb --cli_repo wandb/client-ng.git $* main/
