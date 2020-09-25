3cdd1e44ce5951064aa4f115e473d85d81176bcd
Jeff Raubitschek <jeff@wandb.com>
2020-09-25 09:04:17 -0700

    add tests for fastai v2

---
+++ regression/main/wandb-git/examples/fastai2-segmentation/check.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Common code (refactor into regression framework at some point)
+import os
+import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
+project = os.environ.get("WANDB_PROJECT") or "regression"
+api = wandb.Api()
+last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
+assert last_run.summary_metrics["epoch"] == 4
+assert last_run.summary_metrics["train_loss"] > 0
+assert last_run.summary_metrics["valid_loss"] > 0
+image1 = last_run.summary_metrics["Prediction Samples"]
+assert image1.get("_type").startswith("images")
+assert image1.get("count") == 20
+# TODO: add artifact checks
---
+++ regression/main/wandb-git/examples/fastai2-tabular/check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Common code (refactor into regression framework at some point)
+import os
+import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
+project = os.environ.get("WANDB_PROJECT") or "regression"
+api = wandb.Api()
+last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
+assert last_run.summary_metrics["epoch"] == 2
+assert last_run.summary_metrics["train_loss"] > 0
+assert last_run.summary_metrics["valid_loss"] > 0
+image1 = last_run.summary_metrics["Prediction Samples"]
+assert image1.get("_type") == "table-file"
