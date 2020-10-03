a768fb2bf098734be4c85c811c3df8b4fd028d25
Jeff Raubitschek <jeff@wandb.com>
2020-10-03 08:20:50 -0700

    Add regression test for ray-tune

---
+++ regression/main/wandb-git/examples/ray-tune/check.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Common code (refactor into regression framework at some point)
+import os
+import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
+project = os.environ.get("WANDB_PROJECT") or "regression"
+api = wandb.Api()
+#last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
+project = "{}-raytune-{}".format(project, run_id)
+runs = list(api.runs(project))
+assert len(runs) == 6
+uniq = {(r.config["lr"], r.config["momentum"]) for r in runs}
+assert len(uniq) == 6
+for r in runs:
+    assert(r.summary_metrics["mean_accuracy"] > 0)
