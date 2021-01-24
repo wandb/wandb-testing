b0201e27d992812b59998b9fb514b2d874848a88
Jeff Raubitschek <jeff@wandb.com>
2021-01-24 06:04:13 -0900

    add tf1 tb

---
+++ regression/tests/main/wandb-git/wandb-examples/tf1-summary-writer/check.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Common code (refactor into regression framework at some point)
+import os
+import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
+project = os.environ.get("WANDB_PROJECT") or "regression"
+api = wandb.Api()
+last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
+assert last_run.summary_metrics["global_step"] == 23
+assert last_run.summary_metrics["performance/accuracy"] > 1.0
+assert last_run.summary_metrics["performance/loss"] > 0
