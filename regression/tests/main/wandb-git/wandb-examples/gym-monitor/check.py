4391ccaf7f809b6ee3ba04538094bc316de3cc30
Jeff Raubitschek <jeff@wandb.com>
2021-01-25 09:56:17 -0800

    fix old check script

--- regression/tests/main/wandb-git/wandb-examples/gym-monitor/check.py
+++ regression/tests/main/wandb-git/wandb-examples/gym-monitor/check.py
@@ -3,21 +3,17 @@
 # Common code (refactor into regression framework at some point)
 import os
 import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
 project = os.environ.get("WANDB_PROJECT") or "regression"
-#run_group = os.environ.get("WANDB_RUN_GROUP")
-#job_type = os.environ.get("WANDB_JOB_TYPE")
-name = os.environ.get("WANDB_NAME")
-notes = os.environ.get("WANDB_NOTES")
 api = wandb.Api()
-runs = api.runs(project)
-last_run = next(runs, None)
+last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
 assert last_run, "can not find run"
 assert last_run.state == "finished"
 assert last_run.project == project
-#assert last_run.group == run_group
-#assert last_run.job_type == job_type
-assert last_run.name == name, "Mismatch {} != {}".format(last_run.name, name)
-assert last_run.notes == notes, "Mismatch {} != {}".format(last_run.notes, notes)
 
 #
 # Test Checks
