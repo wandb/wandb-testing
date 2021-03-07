ebf62416a40e4707234972dcfe9bad1a63af4276
Jeff Raubitschek <jeff@wandb.com>
2021-03-07 14:16:43 -0800

    remove key

--- regression/do-cloud-regression.sh
+++ regression/do-cloud-regression.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID=AKIAY775NXAWJSAQYUVQ
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-west-2
-export GOOGLE_APPLICATION_CREDENTIALS=~/gcp-storage-creds-qa.json
+AWS_CREDENTIALS=~/.config/wandb-testing/aws-creds.sh
+source $AWS_CREDENTIALS
+export GOOGLE_APPLICATION_CREDENTIALS=~/.config/wandb-testing/gcp-storage-creds-qa.json
 export GCLOUD_PROJECT=jungle
 time ./do-main-regression.sh $*
