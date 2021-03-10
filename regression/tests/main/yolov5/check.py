6dc49c84b05a06d689d10ca4f6d1ca812483adf5
AYush Chaurasia <ayush.chaurarsia@gmail.com>
2021-03-10 10:56:12 +0000

    Add yolov5 test

---
+++ regression/tests/main/yolov5/check.py
@@ -0,0 +1,12 @@
+import os
+import wandb
+run_id = os.environ.get("WANDB_RUN_ID")
+project = os.environ.get("WANDB_PROJECT") or "regression"
+api = wandb.Api()
+last_run = api.run("%s/%s" % (project, run_id))
+
+#
+# Test Checks
+#
+assert last_run.summary_metrics["metrics/precision"] > 0
+assert last_run.summary_metrics["train/box_loss"] > 0
