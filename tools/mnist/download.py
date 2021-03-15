aa2125ae1753be8ed1d1a6c98c0bc54ff37139c5
Jeff Raubitschek <jeff@wandb.com>
2021-03-15 06:40:02 -0800

    add makeedirs

--- tools/mnist/download.py
+++ tools/mnist/download.py
@@ -11,8 +11,14 @@ if len(args) >= 1:
     download_dir = args[0]
 
 if download_dir:
+    try:
+        os.makedirs(download_dir)
+    except FileExistsError:
+        pass
     os.chdir(download_dir)
 
 os.system("wget -O MNIST.tar.gz https://activeeon-public.s3.eu-west-2.amazonaws.com/datasets/MNIST.new.tar.gz")
+# os.system("wget -O MNIST.tar.gz https://activeeon-public.s3.eu-west-2.amazonaws.com/datasets/MNIST.old.tar.gz")
+
 os.system("tar -zxvf MNIST.tar.gz")
 print("Files downloaded to:", os.path.abspath(os.getcwd()), file=sys.stderr)
