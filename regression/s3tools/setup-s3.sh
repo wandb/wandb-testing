fad71af9494725e22da47ffe93b0a1cbfc842b6c
Jeff Raubitschek <jeff@wandb.com>
2021-03-20 08:29:41 -0800

    fix up s3 testing

--- regression/s3tools/setup-s3.sh
+++ regression/s3tools/setup-s3.sh
@@ -1,10 +1,7 @@
-echo "Setting up minio..."
 set -e
-mkdir -p ~/.aws/
-cp s3tools/aws-credentials.txt ~/.aws/credentials
-cp s3tools/aws-config.txt ~/.aws/config
-aws configure set default.s3.signature_version s3v4
+echo "Setting up minio..."
+./bin/mc alias set s3 http://127.0.0.1:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 set +e
-aws --endpoint-url http://127.0.0.1:9000 s3 mb s3://mybucket
+./bin/mc mb s3/mybucket
 set -e
 echo "done."
--- regression/s3tools/start-s3.sh
+++ regression/s3tools/start-s3.sh
@@ -4,6 +4,11 @@ if [ ! -f bin/minio ]; then
     chmod +x bin/minio.tmp
     mv bin/minio.tmp bin/minio
 fi
+if [ ! -f bin/mc ]; then
+    curl https://dl.minio.io/client/mc/release/linux-amd64/mc -o bin/mc.tmp
+    chmod +x bin/mc.tmp
+    mv bin/mc.tmp bin/mc
+fi
 
 echo "Starting minio..."
 nohup bin/minio server data/ >logs/minio.log 2>&1 &
