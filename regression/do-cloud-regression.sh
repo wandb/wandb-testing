ae3e63707f7e10146c7ca2cdc172c32e9f7a0ba3
Dmitry Duev <dima@wandb.com>
2023-01-17 17:30:49 -0800

    move aws secrets to circle context

--- regression/do-cloud-regression.sh
+++ regression/do-cloud-regression.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Find testing AWS creds in 1password Eng Vault: "wandb-testing: aws-creds.sh"
-AWS_CREDENTIALS=~/.config/wandb-testing/aws-creds.sh
-source $AWS_CREDENTIALS
+#AWS_CREDENTIALS=~/.config/wandb-testing/aws-creds.sh
+#source $AWS_CREDENTIALS
 export GOOGLE_APPLICATION_CREDENTIALS=~/.config/wandb-testing/gcp-storage-creds-qa.json
 export GCLOUD_PROJECT=jungle
 
