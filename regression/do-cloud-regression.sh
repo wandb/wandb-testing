a2f8102df2bdc55ce757e9b3f41f0b1217d2d368
Dmitry Duev <dima@wandb.com>
2023-01-19 13:14:36 -0800

    setup gcp service account

--- regression/do-cloud-regression.sh
+++ regression/do-cloud-regression.sh
@@ -3,8 +3,9 @@
 # Find testing AWS creds in 1password Eng Vault: "wandb-testing: aws-creds.sh"
 #AWS_CREDENTIALS=~/.config/wandb-testing/aws-creds.sh
 #source $AWS_CREDENTIALS
-export GOOGLE_APPLICATION_CREDENTIALS=~/.config/wandb-testing/gcp-storage-creds-qa.json
-export GCLOUD_PROJECT=jungle
+echo $GCP_SERVICE_ACCOUNT_JSON_DECODED > /tmp/gcp-service-account.json
+export GOOGLE_APPLICATION_CREDENTIALS=/tmp/gcp-service-account.json
+export GCLOUD_PROJECT=wandb-client-cicd
 
 EXTRA=${*:-"tests/main/"}
 ./do-main-regression.sh $EXTRA
