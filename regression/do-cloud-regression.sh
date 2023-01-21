a4c3862f733da44825ba6b7cddac73b4457c2fd6
Dmitry Duev <dima@wandb.com>
2023-01-20 17:45:16 -0800

    clean up

--- regression/do-cloud-regression.sh
+++ regression/do-cloud-regression.sh
@@ -10,6 +10,6 @@ export GCLOUD_PROJECT=wandb-client-cicd
 EXTRA=${*:-"tests/main/"}
 parent_dir=$(dirname "$0")
 pushd "$parent_dir"
-do-main-regression.sh $EXTRA
+./do-main-regression.sh $EXTRA
 # the same command, but specifying this file's parent directory as the root
 popd
--- regression/do-sagemaker-regression.sh
+++ regression/do-sagemaker-regression.sh
@@ -4,5 +4,5 @@ export SAGEMAKER_ROLE="arn:aws:iam::618469898284:role/jeff-sagemaker"
 EXTRA=${*:-"tests/sagemaker-beta/"}
 parent_dir=$(dirname "$0")
 pushd "$parent_dir"
-do-cloud-regression.sh $EXTRA
+./do-cloud-regression.sh $EXTRA
 popd
